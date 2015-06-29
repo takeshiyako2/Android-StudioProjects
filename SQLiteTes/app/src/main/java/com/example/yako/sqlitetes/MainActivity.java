dc0ce29e29b4ef91b3f3ac1826f0376aa91291c3
takeshiyako2 <yako.takeshi@googlemail.com>
2015-06-29 14:59:05 +0900

    トグル

--- SQLiteTes/app/src/main/java/com/example/yako/sqlitetes/MainActivity.java
+++ SQLiteTes/app/src/main/java/com/example/yako/sqlitetes/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.yako.sqlitetes;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Collections;
 import java.util.List;
 
 import android.app.Activity;
@@ -15,9 +17,11 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -25,6 +29,9 @@ public class MainActivity extends ActionBarActivity {
     private LinearLayout showData;
     private LinearLayout TextView;
     private EditText dataEdit;
+
+    // Log
+    private final static String TAG = "MainActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,6 +56,41 @@ public class MainActivity extends ActionBarActivity {
 
         // 最新データの表示
         newData();
+
+        // トグル
+        CompoundButton toggle = (CompoundButton)findViewById(R.id.toggle);
+        // トグルのデフォルト
+        // DBからすべてのデータを取得する。
+        List<MyDBEntity> entityList = dao.findAll();
+        // Listの最後の要素を取得
+        MyDBEntity e = entityList.get(entityList.size() - 1);
+        if (e.getValue().equals("isChecked : true")) {
+            toggle.setChecked(true);
+        }
+        Log.i(TAG, e.getValue());
+
+        // トグルの状態変更
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // 状態が変更された
+                Toast.makeText(MainActivity.this, "isChecked : " + isChecked, Toast.LENGTH_SHORT).show();
+
+                // データの追加
+                dao.insert("isChecked : " + isChecked);
+
+                // 入力欄のクリア
+                dataEdit.setText(null);
+
+                // 表示データの更新
+                changeData();
+            }
+        });
+    }
+
+
+    public void onToggleClicked(View view) {
+
     }
 
 
@@ -96,15 +138,17 @@ public class MainActivity extends ActionBarActivity {
         // DBからすべてのデータを取得する。
         List<MyDBEntity> entityList = dao.findAll();
 
+        // リストを降順に並び替えます。
+        Collections.reverse(entityList);
 
-        // データを表示領域に追加する
+                // データを表示領域に追加する
         for(MyDBEntity entity: entityList) {
             TextView textView = new TextView(this);
             textView.setText(entity.getRowId() + "： " + entity.getValue());
             showData.addView(textView);
         }
 
-        // 最新データの表示aaaaa
+        // 最新データの表示
         newData();
     }
 
@@ -122,6 +166,7 @@ public class MainActivity extends ActionBarActivity {
 //        textView.setText(dao.findById(13).getValue());
         textView.setText(e.getValue());
     }
+
 
 
     // ソフトキーボードの「確定」が押された時にソフトキーボードを消す
