9c215c9719205216998000c4458c2f96bc7b26e7
takeshiyako2 <yako.takeshi@googlemail.com>
2015-07-07 18:26:46 +0900

    通知完成

--- Navigation/app/src/main/java/com/example/yako/navigation/AlarmReceiver.java
+++ Navigation/app/src/main/java/com/example/yako/navigation/AlarmReceiver.java
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -21,9 +22,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.List;
+
 
 /**
  * Created by yako on 7/6/15.
@@ -33,29 +33,41 @@ public class AlarmReceiver extends BroadcastReceiver {
     private Context alarmReceiverContext;
     private int notificationProvisionalId;
     private MemoDao dao;
+    private NotificationManager myNotification;
 
     @Override
     public void onReceive(Context context, Intent receivedIntent) {
 
-
-
-
-        // Settingsを確認 ONだったら通知
-        Log.d("onReceive", newData(context));
-        if (newData(context).equals("true")) {
-            Log.d("onReceive", "start notification");
-
-            String message = "test!";
-
+        // Settingsを確認 falseじゃなかったら通知
+        if (newData(context).equals("false")) {
+            // 通知しない
+        }
+        else {
+            // 通知設定
             alarmReceiverContext = context;
             notificationProvisionalId = receivedIntent.getIntExtra("notificationId", 0);
-            NotificationManager myNotification = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            Notification notification = prepareNotification(message);
-            myNotification.notify(notificationProvisionalId, notification);
+            myNotification = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+            // HttpClientの為にスレッド開始
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        // HttpClientで情報取得
+                        HttpClient httpClient = new DefaultHttpClient();
+                        HttpGet httpGet = new HttpGet("http://9post.jp/new-post");
+                        HttpResponse httpResponse = httpClient.execute(httpGet);
+                        String str = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+                        // 通知
+                        Notification notification = prepareNotification(str);
+                        myNotification.notify(notificationProvisionalId, notification);
+                    } catch (Exception ex) {
+                        System.out.println(ex);
+                    }
 
+                }
+            }).start();
         }
-
     }
 
     /**
@@ -88,33 +100,25 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 
     private Notification prepareNotification(String message){
-
-
-
-
-
-        // 通知
-
-        Intent bootIntent =
-                new Intent(alarmReceiverContext, MainActivity.class);
-        PendingIntent contentIntent =
-                PendingIntent.getActivity(alarmReceiverContext, 0, bootIntent, 0);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                alarmReceiverContext);
-        builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+        // 細かい通知設定
+        Intent bootIntent = new Intent(alarmReceiverContext, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(alarmReceiverContext, 0, bootIntent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(alarmReceiverContext);
+//        builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+        builder.setSmallIcon(R.drawable.ic_stat_nine_post_ico)
                 .setTicker(message)
-                .setContentTitle(message)
+                .setContentTitle("9ポスト")
+                .setContentText(message)
                 .setWhen(System.currentTimeMillis())
                 .setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_SOUND)
                 .setContentIntent(contentIntent);
 
-        NotificationCompat.BigPictureStyle pictureStyle =
-                new NotificationCompat.BigPictureStyle(builder);
-        pictureStyle.bigPicture(BitmapFactory.decodeResource(alarmReceiverContext.getResources(), R.drawable.nine_post_icon));
+//        NotificationCompat.BigPictureStyle pictureStyle = new NotificationCompat.BigPictureStyle(builder);
+//        pictureStyle.bigPicture(BitmapFactory.decodeResource(alarmReceiverContext.getResources(), R.drawable.nine_post_icon));
+//        return pictureStyle.build();
 
-        return pictureStyle.build();
-
+        return builder.build();
 
     }
 }
