991c09735f77fb36937f3c84f875580518f3e570
takeshiyako2 <yako.takeshi@googlemail.com>
2015-08-03 14:27:37 +0900

    画像表示

---
+++ Monst/app/src/main/java/nine/post/monst/OpenPlay.java
@@ -0,0 +1,23 @@
+package nine.post.monst;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+
+public class OpenPlay {
+
+    private Context this_context;
+
+    // コンストラクタ
+    public OpenPlay(Context context){
+        this_context = context;
+    }
+
+    // PlayをIntentで開く
+    public void makeIntent() {
+        String facebookUrl = "https://play.google.com/store/apps/details?id=com.monst.news";
+        this_context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl)));
+
+    }
+}
