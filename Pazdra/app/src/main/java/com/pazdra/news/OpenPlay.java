b23dfb6f3d0ab2b9c83e96532cf1e4c1401e2eac
takeshiyako2 <yako.takeshi@googlemail.com>
2015-12-24 16:40:25 +0900

    Pazdra

---
+++ Pazdra/app/src/main/java/com/pazdra/news/OpenPlay.java
@@ -0,0 +1,23 @@
+package com.pazdra.news;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+
+public class OpenPlay {
+
+    private Context this_context;
+
+    // コンストラクタ
+    public OpenPlay(Context context){
+        this_context = context;
+    }
+
+    // PlayをIntentで開く
+    public void makeIntent() {
+        String facebookUrl = "https://play.google.com/store/apps/details?id=com.pazdra.news";
+        this_context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl)));
+
+    }
+}
