abe4b79e9233dbfd5800e8daf12e0098c97e504e
takeshiyako2 <yako.takeshi@googlemail.com>
2017-09-15 14:23:51 +0900

    update monst

--- Monst3/app/src/main/java/nine/post/monst/WebviewFragment.java
+++ Monst3/app/src/main/java/nine/post/monst/WebviewFragment.java
@@ -36,65 +36,23 @@ import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
 
 import static android.R.attr.width;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link WebviewFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link WebviewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WebviewFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
 
     // ログ出力用のタグ
     private static final String TAG = WebviewFragment.class.getSimpleName();
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
     // WebView
     private WebView mWebView;
 
-    // エラーページ
-    private View mErrorPage;
-
     // ページ取得失敗判定
     private boolean mIsFailure = false;
-
-    // ローディングダイアログ
-    private Dialog waitDialog;
-    private Integer sleep_time;
 
     // Five
     private FiveAdCustomLayout Custom;
     private LinearLayout linearLayout;
     private int width;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WebviewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WebviewFragment newInstance(String param1, String param2) {
-        WebviewFragment fragment = new WebviewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     public WebviewFragment() {
         // Required empty public constructor
@@ -103,10 +61,6 @@ public class WebviewFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -121,7 +75,6 @@ public class WebviewFragment extends Fragment {
         // WebViewの設定
         View v = inflater.inflate(R.layout.fragment_webview, container, false);
         mWebView = (WebView) v.findViewById(R.id.webView1);
-//        mWebView.setWebViewClient(new MyWebViewClient(getActivity()));
         WebSettings webSettings = mWebView.getSettings();
         // site_js_flagフラグが1なら、JavaScriptをオン
         if (site_js_flag == 1) {
@@ -134,7 +87,6 @@ public class WebviewFragment extends Fragment {
         mWebView.getSettings().setUseWideViewPort(true);
         mWebView.clearCache(true);
         mWebView.clearHistory();
-        mErrorPage = container.findViewById(R.id.webview_error_page);
         mWebView.setBackgroundColor(0);
         mWebView.loadUrl(url);
 
@@ -158,83 +110,6 @@ public class WebviewFragment extends Fragment {
 
         return v;
     }
-
-    // WebViewClientを継承
-    public class MyWebViewClient extends WebViewClient {
-
-        // エラーが発生した場合
-        @Override
-        public void onReceivedError(WebView webview, int errorCode, String description, String failingUrl) {
-            mIsFailure = true;
-        }
-
-        // ページの読み込み前に呼ばれる
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-
-            // スレッドのローディングダイアログをスタート
-            sleep_time = 1000;
-            try {
-                waitDialog.show();
-                // 実際に行いたい処理は、プログレスダイアログの裏側で行うため、別スレッドにて実行する
-                (new Thread(runnable)).start();
-            } catch (Exception ex) {
-            } finally {
-            }
-
-        }
-
-        // ページ読み込み完了時に呼ばれる
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
-
-        }
-
-        // リンクをタップしたときに呼ばれる
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            Uri uri = Uri.parse(url);
-            String scheme = uri.getScheme();
-            // リンクがニコニコ動画「アプリで再生」の場合
-            if (scheme.indexOf("intent") != -1 && url.indexOf("jp.nicovideo.android") != -1) {
-                Intent intent = null;
-                // インテントを作る
-                try {
-                    intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                }
-                // アクティビティを起動
-                try {
-                    getActivity().startActivityForResult(intent, 12345);
-                } catch (android.content.ActivityNotFoundException e) {
-                    // 指定パッケージのアプリがインストールされていないか
-                    // ACTION_SENDに対応していないか
-                    Toast.makeText(getActivity(), "not installed", Toast.LENGTH_SHORT).show();
-                }
-                return true;
-            }
-            return super.shouldOverrideUrlLoading(view, url);
-        }
-    }
-
-    // スレッドのローディングダイアログを終了
-    private Runnable runnable = new Runnable(){
-        public void run() {
-            // ここではダミーでスリープを行う
-            // 実際にはここに処理を書く
-            try {
-                Thread.sleep(sleep_time);
-            } catch (InterruptedException e) {
-                Log.e("Runnable", "InterruptedException");
-            }
-            // 処理が完了したら、ローディングダイアログを消すためにdismiss()を実行する
-            if (waitDialog != null) {
-                waitDialog.dismiss();
-            }
-        }
-    };
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
